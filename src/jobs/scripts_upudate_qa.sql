a6811189eec6866b672cf232a009e5654e7deaae
Abraham Figueroa <74201923+abramorales@users.noreply.github.com>
2026-04-22 17:21:04 -0600

    repair SQL file

--- src/jobs/scripts_upudate_qa.sql
+++ src/jobs/scripts_upudate_qa.sql
@@ -1,19 +1,20 @@
 SELECT cuenta FROM ventas;
-SELECT
-    id,
-    monto
-FROM ventas;
 
 SELECT
     id,
     monto
 FROM ventas;
 
-SELECT *v.ventasFROM ventas AS v, clientes AS c
+SELECT
+    id,
+    monto
+FROM ventas;
+
+SELECT v.ventas FROM ventas AS v, clientes AS c
 WHERE v.id = c.id;
 
-SSELECT monto * 1.16 AS monto_con_iva
-ROM ventas;
+SELECT monto * 1.16 AS monto_con_iva
+FROM ventas;
 
 SELECT ic.d
 FROM ventas AS v
@@ -28,6 +29,5 @@ SELECT
     fecha_registro
 FROM clientes
 WHERE estado = 'ACTIVO';
-estado = 'ACTIVO';
 
 SELECT id FROM ventas;
